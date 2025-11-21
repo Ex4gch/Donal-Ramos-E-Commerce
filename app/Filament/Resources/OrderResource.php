c582f6d93428226c607fa7f21062c20a46b47dea
ex4gch <toounit@gmail.com>
2025-11-21 14:41:03 +0800

    6th Video - Address Resource

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Filament\Resources\OrderResource\RelationManagers\AddressRelationManager;
 use App\Models\Order;
 use Filament\Actions\SelectAction;
 use Filament\Forms;
@@ -265,7 +266,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            AddressRelationManager::class,
         ];
     }
 
