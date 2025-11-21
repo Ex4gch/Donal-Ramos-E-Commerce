e2932c79ef6fb196a80e6804fff32979fe9b62c3
ex4gch <toounit@gmail.com>
2025-11-21 14:59:46 +0800

    7th Video - Widgets

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
 
 class ListOrders extends ListRecords
 {
@@ -16,4 +17,24 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderResource\Widgets\OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All')->label('All Orders'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status', 'new')),
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered')),
+            'canceled' => Tab::make()->query(fn ($query) => $query->where('status', 'canceled')),
+        ];
+    }
+
 }
